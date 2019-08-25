8a947dbb54deaa0a61faa7b0c4f6a8a74a3af482
Hui Cao <nonocast@gmail.com>
2019-08-25 15:33:15 +0800

    fix

--- BST-M51/src/app.c
+++ BST-M51/src/app.c
@@ -1,5 +1,7 @@
 #include <8052.h>
 
+unsigned char codes[17] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x6f, 0x77, 0x7c, 0x39, 0x5e, 0x79, 0x71, 0x00};
+
 void delay(unsigned int i) {
   unsigned char j;
   for (i; i > 0; i--)
@@ -7,11 +9,29 @@ void delay(unsigned int i) {
       ;
 }
 
+// light(~(0x01 << (i%8)))
+void light(int i) {
+  P1 = i;
+}
+
+void beep(int i) {
+  P2_3 = i;
+}
+
+void led(unsigned char c) {
+  P2_4 = 1;
+  P2_5 = 1;
+  P2_6 = 1;
+  P2_7 = 1;
+  P0 = c;
+}
+
 void main() {
-  while (1) {
-    P1 = 0x55;
+  int i = 0;
+  while (++i) {
     delay(1000);
-    P1 = 0xAA;
-    delay(1000);
+    beep(i % 2);
+    led(codes[i % 17]);
+    light(~(0x01 << (i % 8)));
   }
 }
